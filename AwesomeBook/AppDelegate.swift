1c4775fbb2978c0f9b973554c891f3157fc80785
Geektree0101 <h2s1880@gmail.com>
2021-09-26 22:50:22 +0900

    update AwesomeBookAppDelegate

--- AwesomeBook/AppDelegate.swift
+++ AwesomeBook/AppDelegate.swift
@@ -5,23 +5,25 @@
 //  Created by Geektree0101 on 2021/09/26.
 //
 
-import Foundation
 import UIKit
 import SwiftUI
 
+import NeedleFoundation
 import CodyFire
 
 final class AwesomeBookAppDelegate: NSObject, UIApplicationDelegate {
   
-  var rootComponent: RootComponent!
-  var window: UIWindow?
-  var router = Router()
+  let rootComponent: RootComponent
+  let router = Router()
+
+  override init() {
+    registerProviderFactories()
+    self.rootComponent = RootComponent()
+    super.init()
+  }
   
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    
-    registerProviderFactories()
-    self.rootComponent = RootComponent()
     self.rootComponent.networking.configure()
     self.router.application = application
     return true
